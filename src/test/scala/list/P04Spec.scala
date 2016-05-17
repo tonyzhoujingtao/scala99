3247b6318aea88a9b5e2373ee69f9ba2a4e17ff8
Tony ZHOU <zjt1114@gmail.com>
2016-05-17 13:44:09 -0700

    REFACTOR: remove unused comments

--- src/test/scala/list/P04Spec.scala
+++ src/test/scala/list/P04Spec.scala
@@ -15,9 +15,4 @@ class P04Spec extends FlatSpec with ShouldMatchers {
     measure(list) should be(1)
   }
 
-  //  it must "return the list in reverse if there are more than 1 elements in the list" in {
-  //    val list = List(1, 2)
-  //    reverse(list) should be(List(2, 1))
-  //  }
-
 }
