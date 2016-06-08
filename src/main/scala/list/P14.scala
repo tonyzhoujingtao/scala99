01dea7519b81aa5a8a7afab5d5a2eb22fa5fd801
Tony Zhou <tzhou@twitter.com>
2016-06-07 21:37:43 -0600

    ADD: P14

--- src/main/scala/list/P14.scala
+++ src/main/scala/list/P14.scala
@@ -2,9 +2,6 @@ package list
 
 /**
  * P14 (*) Duplicate the elements of a list.
- *
- * Implement the so-called run-length encoding data compression method directly.
- * I.e. don't use other methods you've written (like P09's pack); do all the work directly.
  */
 object P14 {
 
